a99597d5cba9c8eac89f2141878a0608e4542790
David García <davidgf1987@gmail.com>
2015-02-21 15:36:59 +0100

    Mailer fix

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 require 'yaml'
 APP_CONFIG = YAML.load(File.read(Rails.root.join("config/app_config.yml")))
-
 # Initialize the Rails application.
 Droplayer::Application.initialize!
